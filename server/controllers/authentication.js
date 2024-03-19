bac77b4fcaa6dcdf5959b83f89ed1a1ec56a8187
Vagdevi_yalla <yallavagdevi@gmail.com>
2024-03-19 21:48:16 +0530

    added changes to controllers

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,15 +1,15 @@
-// const User = require("../models/userModel");
+const User = require("../models/userModel");
 
 const register = async (req, res) => {
   try {
     const { name, email, phoneNumber, password } = req.body;
     console.log(req.body);
     if (!name || !email || !phoneNumber || !password) {
-      return res.json("Please fill all the details");
+      return res.json("please fill the details");
     }
     const newUser = new User({ name, email, phoneNumber, password });
     await newUser.save();
-    res.json("User registered successfully");
+    res.status(201).json(newUser);
   } catch (error) {
     res.status(500).json(error);
   }
@@ -19,22 +19,12 @@ const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      return res.send("Please fill all the details");
+      return res.send("please fill the details");
     }
     const user = await User.findOne({ email });
     if (!user) {
-      return res.json("Email not found");
+      return res.status(404).json("Email not found");
     }
-    // Here you should add logic to check if the password matches
-    // with the password stored in the database for the user.
-    // Example:
-    // const isPasswordValid = await user.comparePassword(password);
-    // if (!isPasswordValid) {
-    //   return res.json("Invalid password");
-    // }
-    // If the password is valid, you can proceed with login.
-    // For now, let's assume login is successful without password verification.
-    res.json("Login successful");
   } catch (error) {
     res.status(500).json(error);
   }
